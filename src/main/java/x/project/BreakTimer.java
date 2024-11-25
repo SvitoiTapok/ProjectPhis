5dc54a0cc61e219047b449acf89dc88dedff6909
vvlaads <vvlaads@gmail.com>
2024-11-25 03:12:23 +0300

    Create BreakTimer.java

---
+++ src/main/java/x/project/BreakTimer.java
@@ -0,0 +1,47 @@
+package x.project;
+
+import javafx.animation.AnimationTimer;
+
+public class BreakTimer extends AnimationTimer {
+    private long lastTime = 0;
+    private boolean stop = false;
+    private final Physics physics;
+
+    public BreakTimer(Physics physics) {
+        this.physics = physics;
+    }
+
+    @Override
+    public void handle(long now) {
+        if (!stop) {
+            if (lastTime == 0) {
+                physics.doFrame(0);
+                lastTime = now;
+                return;
+            }
+            physics.doFrame((now - lastTime) / 1.0e9);
+        }
+        lastTime = now;
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+    }
+
+    public long getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(long lastTime) {
+        this.lastTime = lastTime;
+    }
+
+    public Physics getPhysics() {
+        return physics;
+    }
+}
+
